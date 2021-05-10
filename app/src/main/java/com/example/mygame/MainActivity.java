7bd5d624bff3586ba0756820883c5a458f9354e3
SysoevAlx <sashasis45@gmail.com>
2021-05-10 22:41:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mygame/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.mygame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        hideNavBar();
+    }
+
+    // Скрыть системную навигацию при разворачивании приложения
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideNavBar();
+    }
+
+    // Скрыть системную навигацию
+    private void hideNavBar() {
+
+
+        this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
+    }
+}
